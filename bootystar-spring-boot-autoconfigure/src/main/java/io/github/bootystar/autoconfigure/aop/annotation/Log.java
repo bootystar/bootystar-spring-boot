04fa35baaa1d7b58cf54087bd6cc14a35d70a4c3
lo <604647004@qq.com>
2025-10-17 09:13:03 +0800

    日志切面

--- bootystar-spring-boot-autoconfigure/src/main/java/io/github/bootystar/autoconfigure/aop/annotation/Log.java
+++ bootystar-spring-boot-autoconfigure/src/main/java/io/github/bootystar/autoconfigure/aop/annotation/Log.java
@@ -1,8 +1,14 @@
 package io.github.bootystar.autoconfigure.aop.annotation;
 
+import java.lang.annotation.*;
+
 /**
- *
+ * 日志记录
  * @author bootystar
  */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Log {
+    
 }
